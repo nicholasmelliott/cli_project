8cc3ba5a587c3b9ccfe5f44d9a1e97b337a62782
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2026-03-03 09:22:47 -0500

    Fix spelling error

--- mcp_server.py
+++ mcp_server.py
@@ -6,7 +6,7 @@ mcp = FastMCP("DocumentMCP", log_level="ERROR")
 
 
 docs = {
-    "deposition.md": "This deposition, taken by John Smith, covers the testimony of Angela Smith, P.E. Further depostions from the same parties are required by the 3rd of April.",
+    "deposition.md": "This deposition, taken by John Smith, covers the testimony of Angela Smith, P.E. Further depositions from the same parties are required by the 3rd of April.",
     "report.pdf": "The report details the state of a 20m condenser tower.",
     "financials.docx": "These financials outline the project's budget and expenditures.",
     "outlook.pdf": "This document presents the projected future performance of the system.",
