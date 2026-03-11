d32b821f44823e9fae381b524de01ea762b3ba01
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2026-03-11 07:29:57 -0400

    Import docs variable from core.docs_data module

---
+++ core/docs_data.py
@@ -0,0 +1,8 @@
+docs = {
+    "deposition.md": "This deposition, taken by John Smith, covers the testimony of Angela Smith, P.E. Further depositions from the same parties are required by the 3rd of April.",
+    "report.pdf": "The report details the state of a 20m condenser tower.",
+    "financials.docx": "These financials outline the project's budget and expenditures.",
+    "outlook.pdf": "This document presents the projected future performance of the system.",
+    "plan.md": "The plan outlines the steps for the project's implementation. Completion of the plan must occur by May, 15 2026.",
+    "spec.txt": "These specifications define the technical requirements for the equipment.",
+}
